d965e258aa60078084e48eaf5e88e3f0c2708c38
allanfoppa <allanfoppa@hotmail.com>
2026-04-17 12:55:20 -0300

    Random repo cards in UI

--- src/components/Section.ts
+++ src/components/Section.ts
@@ -5,7 +5,7 @@ export function Section(repos: RepoItem[], query: string): string {
   return `
     <section>
       <div class="grid gap-4">
-        ${repos.map(repo => RepoCard(repo, query)).join('')}
+        ${repos.map(repo => RepoCard(repo, query)).sort(() => Math.random() - 0.5).join('')}
       </div>
     </section>
   `
